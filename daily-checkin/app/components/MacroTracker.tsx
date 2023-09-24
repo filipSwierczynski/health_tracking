d17bf68e3ec754926e73b2a190a4063bb2f925f0
Filip Swierczynski <swirek.gw@hotmail.com>
2023-09-23 20:06:27 -0400

    feature - input calories, calculate total intake

--- daily-checkin/app/components/MacroTracker.tsx
+++ daily-checkin/app/components/MacroTracker.tsx
@@ -1,43 +1,56 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 interface Props { 
     greeting: string;
-    
 }
-const defaultInputMacros = {
-    proteinIntake: '',
-    carbsIntake: '',
-    fatIntake: ''
-}
+
 
 const MacroTracker = (props: Props) => {
    
+    const [prescribedMacros, setPrescribedMacros] = useState({
+        protein: '', carbs: '', fat: ''
+    })
 
-    const [inputMacros, setInputMacros] = useState(defaultInputMacros)
+    const [inputMacros, setInputMacros] = useState({proteinIntake: 0,
+    carbsIntake: 0,
+    fatIntake: 0});
+    const [totalInputCalories, setTotalInputCalories] = useState(0);
 
-    const handleChange = (e: any) => {
-        const { name, value } = e.target;
+    
+    const handleMacroChange = (e: any) => {
         setInputMacros({
             ...inputMacros,
-            [name]: value,
+            [e.target.name]: e.target.value,
         })
+    }
+
+    useEffect(()=> {
+        setTotalInputCalories(calculateTotalIntakeCalories(inputMacros.proteinIntake,inputMacros.carbsIntake,inputMacros.fatIntake))
+    },[inputMacros.proteinIntake,inputMacros.carbsIntake,inputMacros.fatIntake])
+
+    const calculateTotalIntakeCalories = (protein:number,carbs:number,fat:number) => { 
+        const total = (protein * 4)  + (carbs*4)  + (fat * 9);
+        console.log(total);
+        return total;
     }
   return (
     <>
     <div>
-        <input name="proteinIntake" placeholder='Grams of proteins' id='proteinIntake' value={defaultInputMacros.proteinIntake} onChange={handleChange}></input>
-        <input name="carbsIntake" placeholder='Grams of carbohydrates' id='carbsIntake' value={defaultInputMacros.carbsIntake} onChange={handleChange}></input>
-        <input name="fatIntake" placeholder='Grams of fats' id="fatsIntake" onChange={handleChange} value={defaultInputMacros.fatIntake}></input>
+        <p>Enter your macros for today</p>
+        <input name="proteinIntake" placeholder='Grams of proteins' id='proteinIntake'  onChange={handleMacroChange}></input>
+        <input name="carbsIntake" placeholder='Grams of carbohydrates' id='carbsIntake'  onChange={handleMacroChange}></input>
+        <input name="fatIntake" placeholder='Grams of fats' id="fatsIntake" onChange={handleMacroChange} ></input>
     </div>
+    {inputMacros.proteinIntake} <br/>
+    {inputMacros.carbsIntake} <br/>
+    {inputMacros.fatIntake} <br/>
+    <p>total intake calories: {totalInputCalories}</p>
+    <div>
+        <p>Prescribed macros</p>
 
-    {inputMacros.proteinIntake}
-    {inputMacros.carbsIntake}
-    {inputMacros.fatIntake}
+    </div>
     </>
-    
-    
   )
 }
-
 export default MacroTracker
